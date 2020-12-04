7d87354b64f85d052efef93826ceff449e322844
Michael Seifarth <mseifarth@babbel.com>
2020-12-05 00:32:55 +0100

    day 3 part 2

--- src/days/3.js
+++ src/days/3.js
@@ -1,21 +1,29 @@
-const TREE = '#';
+const TREE = "#";
+const slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
 
-module.exports = (input) => {
-  let x = 0;
-  let treesEncountered = 0;
+module.exports = (input) =>
+  slopes.reduce((acc, [deltaX, deltaY], index) => {
+    let x = 0;
+    let treesEncountered = 0;
 
-  for (let i = 0; i < input.length; i++) {
-    let pos = input[i][x];
+    for (let i = 0; i < input.length; i += deltaY) {
+      let pos = input[i][x];
 
-    if (pos === TREE) {
-      treesEncountered++;
+      if (pos === TREE) {
+        treesEncountered++;
+      }
+
+      x += deltaX;
+      if (x >= input[i].length) {
+        x -= input[i].length;
+      }
     }
 
-    x += 3;
-    if (x >= input[i].length) {
-      x -= input[i].length;
-    }
-  }
-
-  return treesEncountered;
-};
+    return acc * treesEncountered;
+  }, 1);
